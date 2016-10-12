00a272264e115d49a46208540329c4fe553c515c
Calder Phillips-Grafflin <calder.pg@gmail.com>
2016-10-12 17:23:50 -0400

    Adapted controllers for running onboard the robot and added support for both arms

---
+++ src/baxter_left_arm_velocity_torque_controller.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+#include <random>
+#include <Eigen/Geometry>
+#include <time.h>
+#include <chrono>
+#include <ros/ros.h>
+#include <baxter_robot_controllers/baxter_robot_velocity_torque_controller.hpp>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "baxter_left_arm_velocity_torque_controller");
+    ROS_INFO("Starting baxter_left_arm_velocity_torque_controller...");
+    ros::NodeHandle nh;
+    ros::NodeHandle nhp("~");
+    const std::string DEFAULT_TARGET_VELOCITY_TOPIC = "/robot/limb/left/joint_command_raw";
+    const std::string DEFAULT_CONFIG_FEEDBACK_TOPIC = "/robot/joint_states";
+    const std::string DEFAULT_JOINT_COMMAND_TOPIC = "/robot/limb/left/joint_command";
+    const std::string DEFAULT_ABORT_SERVICE = "baxter_left_arm_velocity_torque_controller/abort";
+    const double DEFAULT_CONTROL_RATE = 1000.0; //25.0;
+    const std::string target_velocity_topic = nhp.param(std::string("target_velocity_topic"), DEFAULT_TARGET_VELOCITY_TOPIC);
+    const std::string joint_command_topic = nhp.param(std::string("joint_command_topic"), DEFAULT_JOINT_COMMAND_TOPIC);
+    const std::string config_feedback_topic = nhp.param(std::string("config_feedback_topic"), DEFAULT_CONFIG_FEEDBACK_TOPIC);
+    const std::string abort_service = nhp.param(std::string("abort_service"), DEFAULT_ABORT_SERVICE);
+    const double control_rate = nhp.param(std::string("control_rate"), DEFAULT_CONTROL_RATE);
+    // Get the XML string of the URDF
+    const std::string xml_model_string = nh.param(std::string("robot_description"), std::string(""));
+    // Joint limits
+    const std::map<std::string, baxter_robot_controllers::JointLimits> joint_limits = baxter_robot_controllers::GetLeftArmLimits();
+    // Joint PID params
+    const std::map<std::string, baxter_robot_controllers::PIDParams> joint_controller_params = baxter_robot_controllers::GetLeftArmDefaultPositionControllerParams();
+    baxter_robot_controllers::BaxterRobotVelocityTorqueController controller(nh, target_velocity_topic, config_feedback_topic, joint_command_topic, abort_service, xml_model_string, joint_limits, joint_controller_params);
+    ROS_INFO("...startup complete");
+    controller.Loop(control_rate);
+    return 0;
+}
---
+++ src/baxter_right_arm_position_controller.cpp
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+#include <random>
+#include <Eigen/Geometry>
+#include <time.h>
+#include <chrono>
+#include <ros/ros.h>
+#include <baxter_robot_controllers/baxter_robot_position_controller.hpp>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "baxter_right_arm_position_controller");
+    ROS_INFO("Starting baxter_right_arm_position_controller...");
+    ros::NodeHandle nh;
+    ros::NodeHandle nhp("~");
+    const std::string DEFAULT_TARGET_CONFIG_TOPIC = "baxter_right_arm_position_controller/target";
+    const std::string DEFAULT_STATUS_TOPIC = "baxter_right_arm_position_controller/status";
+    const std::string DEFAULT_CONFIG_FEEDBACK_TOPIC = "/robot/joint_states";
+    const std::string DEFAULT_JOINT_COMMAND_TOPIC = "/robot/limb/right/joint_command_raw";
+    const std::string DEFAULT_ABORT_SERVICE = "baxter_right_arm_position_controller/abort";
+    const double DEFAULT_CONTROL_RATE = 100.0; //25.0;
+    const double DEFAULT_VELOCITY_LIMIT_SCALING = 0.75;
+    std::string target_config_topic;
+    std::string joint_command_topic;
+    std::string config_feedback_topic;
+    std::string status_topic;
+    std::string abort_service;
+    double control_rate = DEFAULT_CONTROL_RATE;
+    double velocity_limit_scaling = DEFAULT_VELOCITY_LIMIT_SCALING;
+    nhp.param(std::string("target_config_topic"), target_config_topic, DEFAULT_TARGET_CONFIG_TOPIC);
+    nhp.param(std::string("config_feedback_topic"), config_feedback_topic, DEFAULT_CONFIG_FEEDBACK_TOPIC);
+    nhp.param(std::string("joint_command_topic"), joint_command_topic, DEFAULT_JOINT_COMMAND_TOPIC);
+    nhp.param(std::string("status_topic"), status_topic, DEFAULT_STATUS_TOPIC);
+    nhp.param(std::string("abort_service"), abort_service, DEFAULT_ABORT_SERVICE);
+    nhp.param(std::string("control_rate"), control_rate, DEFAULT_CONTROL_RATE);
+    nhp.param(std::string("velocity_limit_scaling"), velocity_limit_scaling, DEFAULT_VELOCITY_LIMIT_SCALING);
+    const double real_velocity_limit_scaling = std::max(0.0, std::min(1.0, std::abs(velocity_limit_scaling)));
+    // Joint limits
+    const std::map<std::string, baxter_robot_controllers::JointLimits> joint_limits = baxter_robot_controllers::GetRightArmLimits(real_velocity_limit_scaling);
+    // Joint PID params
+    const std::map<std::string, baxter_robot_controllers::PIDParams> joint_controller_params = baxter_robot_controllers::GetRightArmDefaultPositionControllerParams();
+    baxter_robot_controllers::BaxterRobotPositionController controller(nh, target_config_topic, config_feedback_topic, joint_command_topic, status_topic, abort_service, joint_limits, joint_controller_params);
+    ROS_INFO("...startup complete");
+    controller.Loop(control_rate);
+    return 0;
+}
